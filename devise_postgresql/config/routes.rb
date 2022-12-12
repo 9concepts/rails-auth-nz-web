8399172ab9326976cbe22291c18deafa3fd72454
9sako6 <31821663+9sako6@users.noreply.github.com>
2022-12-12 20:09:41 +0900

    ログアウトできるようにした

--- devise_postgresql/config/routes.rb
+++ devise_postgresql/config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users, skip: [:sessions]
   root "top#index"
 
   devise_scope :user do
     get 'login', to: 'users/sessions#new', as: :new_user_session
     post 'login', to: 'users/sessions#create', as: :user_session
+    delete 'logout', to: 'users/sessions#destroy', as: :destroy_user_session
   end
 end
